f1a10baf8517ad5095db49ef023cdb75c32e2374
bakarytoure <bakarytoure57@gmail.com>
2020-07-08 23:48:41 +0000

    Updates

---
+++ precache-manifest.86eafe60e719c6c7d4e0c99d08e2823f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1675defa3715ac0d94e5f571a7c305f",
+    "url": "/add-or-remove-item-to-the-list/index.html"
+  },
+  {
+    "revision": "71ff6148efa970343f06",
+    "url": "/add-or-remove-item-to-the-list/static/css/main.5d8a0be8.chunk.css"
+  },
+  {
+    "revision": "bc9063b120547c8a4e76",
+    "url": "/add-or-remove-item-to-the-list/static/js/2.e99c338e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/add-or-remove-item-to-the-list/static/js/2.e99c338e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ff6148efa970343f06",
+    "url": "/add-or-remove-item-to-the-list/static/js/main.cb702671.chunk.js"
+  },
+  {
+    "revision": "51166bc1752217c69dd3",
+    "url": "/add-or-remove-item-to-the-list/static/js/runtime-main.77b4ea91.js"
+  }
+]);
